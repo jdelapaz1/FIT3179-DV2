7032d7edf4b101b0f913fd03d4d56f29912ee228
jdelapaz1 <167676416+jdelapaz1@users.noreply.github.com>
2025-10-17 07:32:20 +1100

    Initial commit

---
+++ vega_lite_vis.js
@@ -0,0 +1,25 @@
+var vg_1 = "map_tournament_type.json";
+var vg_2 = "map_players.json";
+var vg_3 = "countries_winning_percentage.json"
+var vg_4 = "countries_most_titles.json"
+var vg_5 = "avg_minute_set.json"
+var vg_6 = "retirements_walkovers.json"
+var vg_7 = "upsets_by_surface.json"
+var vg_8 = "top10_by_country.json"
+var vg_9 = "continent_matches_played_vs_won.json"
+var vg_10 = "titles_by_country.json"
+var vg_11 = "ranking_match_duration.json"
+var vg_12 = "walkovers_retirements.json"
+
+vegaEmbed("#map_tournament_type", vg_1).then(function(result){}).catch(console.error);
+vegaEmbed("#map_players", vg_2).then(function(result){}).catch(console.error);
+vegaEmbed("#countries_winning_percentage", vg_3).then(function(result){}).catch(console.error);
+vegaEmbed("#countries_most_titles", vg_4).then(function(result){}).catch(console.error);
+vegaEmbed("#avg_minute_set", vg_5).then(function(result){}).catch(console.error);
+vegaEmbed("#retirements_walkovers", vg_6).then(function(result){}).catch(console.error);
+vegaEmbed("#upsets_by_surface", vg_7).then(function(result){}).catch(console.error);
+vegaEmbed("#top10_by_country", vg_8).then(function(result){}).catch(console.error);
+vegaEmbed("#matches_played_vs_won", vg_9).then(function(result){}).catch(console.error);
+vegaEmbed("#titles_by_country", vg_10).then(function(result){}).catch(console.error);
+vegaEmbed("#ranking_match_duration", vg_11).then(function(result){}).catch(console.error);
+vegaEmbed("#walkovers_retirements", vg_12).then(function(result){}).catch(console.error);
